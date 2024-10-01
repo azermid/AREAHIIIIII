5bea23b0d50389cc047c425b50c75f9139276967
NoxLoveYa <ewan.massonet@epitech.eu>
2024-10-01 17:11:23 +0200

    [ADD] Correct database creatte script

--- SQL/Database_create.sql
+++ SQL/Database_create.sql
@@ -1,18 +1,21 @@
 CREATE DATABASE IF NOT EXISTS Area;
 USE Area;
 
--- User table
 -- DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     oauth_provider VARCHAR(255), -- e.g., 'google', 'facebook', etc.
     oauth_id VARCHAR(255), -- stores the ID from the OAuth provider
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE (username, email, oauth_provider)
 );
 
-INSERT INTO users (username, password)
-VALUES ('testuser', 'password');
+-- Insert a test user
+INSERT INTO users (username, email, password, oauth_provider, oauth_id)
+VALUES ('testuser', 'testuser@example.com', 'password', 'area', '0');
+--need to hash pass
 
--- select * from users;
+--SELECT * FROM users;
