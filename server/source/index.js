b9c54255d6cbd572717479e1530ac41a291c6da5
Yann Malaret <yann.malaret@epitech.eu>
2024-10-29 19:30:20 +0100

    [ADD] patch method

--- server/source/index.js
+++ server/source/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   const allowedOrigin = 'http://localhost:8081';
   res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, ngrok-skip-browser-warning');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
