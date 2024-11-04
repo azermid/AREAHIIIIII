6e4222bc43176e9495a66cb49f63cce99183b16c
NoxLoveYa <ewan.massonet@epitech.eu>
2024-11-05 00:39:04 +0100

    [ADD] githu in progress

--- server/source/routes/WebhookRoutes.js
+++ server/source/routes/WebhookRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const GmailController = require('../controllers/GmailController');
+const GithubController = require('../controllers/GithubController');
 
 module.exports = (dbConnection) => {
     const router = express.Router();
 
     const gmailController = new GmailController(dbConnection);
+    const githubController = new GithubController(dbConnection);
 
-    router.post('/gmail/register', (req, res) => gmailController.register(req, res));
     router.post('/gmail', (req, res) => gmailController.watch(req, res));
 
+    router.post('/github', (req, res) => githubController.watch(req, res));
     return router;
 }
