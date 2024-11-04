1b1440068d30f20f1d25f393b278309f708cb5d1
Yann Malaret <yann.malaret@epitech.eu>
2024-11-04 18:56:55 +0100

    [ADD] action reaction poll for outlook

---
+++ server/source/reactions/send_email_outlook.js
@@ -0,0 +1,32 @@
+async function send_email_outlook(token, data, email) {
+    const { to, text, subject } = data;
+    console.log('Sending email to:', to);
+    console.log('Email subject:', subject);
+    console.log('Email text:', text);
+    const response = await fetch(
+        "https://graph.microsoft.com/v1.0/me/sendMail",
+        {
+            method: "POST",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                message: {
+                    subject,
+                    body: {
+                        contentType: "Text",
+                        content: text,
+                    },
+                    toRecipients: [{ emailAddress: { address: to } }],
+                },
+            }),
+        }
+    );
+    if (response.status !== 202) {
+        console.error('Error:', response.statusText);
+        return;
+    }
+}
+
+module.exports = send_email_outlook;
--- server/source/worker/OutlookWorker.js
+++ server/source/worker/OutlookWorker.js
@@ -1,9 +1,11 @@
 const ActionRepository = require('../repositories/ActionRepository')
+const ReactionRepository = require('../repositories/ReactionRepository')
 const TriggerRepository = require('../repositories/TriggerRepository')
 
 async function pollOutlookForNewEmails(db, interval) {
     console.log('Polling Outlook for new emails...');
     const actionRepository = new ActionRepository(db);
+    const reactionRepository = new ReactionRepository(db);
     const triggerRepository = new TriggerRepository(db);
 
     const action_id = await actionRepository.getIdByName('new_email_outlook');
@@ -20,23 +22,37 @@ async function pollOutlookForNewEmails(db, interval) {
             }
         );
         const data = await response.json();
-        console.log('Data:', data);
-        // server-1         |       receivedDateTime: '2024-10-29T19:17:01Z',
-        // make sure to filter out emails that have already been processed by checking the receivedDateTime
-        //process from now - interval
+        if (data.error) {
+            console.error('Error:', data.error);
+            return;
+        }
         const processFrom = new Date(Date.now() - interval);
         console.log('Process from:', processFrom);
 
         for (const email of data.value) {
-            // check if email has already been processed (check date)
-            // if not, process email
+            if (email.receivedDateTime < processFrom.toISOString()) {
+                // console.log('Skipping email:', email);
+                continue;
+            }
+            // console.log('Processing email:', email);
+            const action_data = trigger.action_data;
+            if (email.from.emailAddress.address !== action_data.from) {
+                // console.log('Skipping email:', email);
+                continue;
+            }
+            console.log("email matches trigger");
+            const reactionName = await reactionRepository.getNameById(trigger.reaction_id);
+            console.log("reaction name", reactionName);
+            //send_email_outlook
+            const reaction = require(`../reactions/${reactionName}.js`);
+            await reaction(trigger.action_service_token, trigger.reaction_data, email);
+            //do reaction
         }
     }
 }
 
 function startOutlookPollingWorker(interval, db) {
     setInterval(async () => {
-        console.log('Polling Outlook for new emails...');
         await pollOutlookForNewEmails(db, interval);
     }, interval);
 }
