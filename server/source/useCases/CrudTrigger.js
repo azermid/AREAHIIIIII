3942a5389e06eca1f1f7edb8e24eae6b7cbd70cb
NoxLoveYa <ewan.massonet@epitech.eu>
2024-11-05 03:15:59 +0100

    [FIX] Yann retarded undefined crudTrigger ?????????

--- server/source/useCases/CrudTrigger.js
+++ server/source/useCases/CrudTrigger.js
@@ -57,9 +57,9 @@ class CrudTrigger {
             throw new Error('Workspace_id, type, action_id, reaction_id, action_data, reaction_data, action_service_token, reaction_service_token are required');
         }
         if (action_service_refresh_token == undefined)
-            action_service_refresh_token = "fyghjk";
+            action_service_refresh_token = "undefined";
         if (reaction_service_refresh_token == undefined)
-            reaction_service_refresh_token = "esrdftyghuijok";
+            reaction_service_refresh_token = "undefined";
         const triggerExists = await this.getByWorkspaceId({workspace_id});
         if (triggerExists.length > 0) {
             const trigger = new Trigger({ id: triggerExists[0].id, workspace_id, type, action_id, reaction_id, action_data, reaction_data, action_service_token, reaction_service_token, action_service_refresh_token, reaction_service_refresh_token, webhook_url, webhook_secret });
