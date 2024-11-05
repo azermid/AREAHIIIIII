bbc374d56164fd1cca886d7a9c4e0889b3857553
Amir Madaoui <amir.madaoui@epitech.eu>
2024-11-05 01:40:03 +0100

    [fix] github service

--- server/source/services/GitHubAuth.js
+++ server/source/services/GitHubAuth.js
@@ -1,6 +1,4 @@
 const fetch = require('node-fetch');
-const jwt = require('jsonwebtoken');
-const fs = require('fs');
 
 class GitHubAuth {
     constructor(thirdPartyLogin) {
@@ -8,20 +6,8 @@ class GitHubAuth {
         this.clientId = process.env.GITHUB_CLIENT_ID;
         this.clientSecret = process.env.GITHUB_CLIENT_SECRET;
         this.redirectURI = null;
-        this.privateKeyPath = process.env.GITHUB_PRIVATE_KEY_PATH;
-        this.appId = process.env.GITHUB_APP_ID;
         this.serviceName = 'Area-F';
         this.serviceType = 'oauth';
-    }
-
-    generateJWT() {
-        const privateKey = fs.readFileSync(this.privateKeyPath, 'utf8');
-        const payload = {
-            iat: Math.floor(Date.now() / 1000),
-            exp: Math.floor(Date.now() / 1000) + (10 * 60),
-            iss: this.appId,
-        };
-        return jwt.sign(payload, privateKey, { algorithm: 'RS256' });
     }
 
     getAuthUrl() {
@@ -33,17 +19,18 @@ class GitHubAuth {
             'admin:gpg_key', 'codespace'
         ].join(',');
 
-        const authUrl = `https://github.com/login/oauth/authorize?client_id=${this.clientId}&redirect_uri=${encodeURIComponent(process.env.GITHUB_REDIRECT_URI)}&scope=${scopes}`;
-        return authUrl;
+        return `https://github.com/login/oauth/authorize?client_id=${this.clientId}` +
+        `&redirect_uri=${encodeURIComponent(process.env.GITHUB_REDIRECT_URI)}` +
+        `&scope=${scopes}`;
     }
-$
+
     async getGitHubTokens(code) {
         const tokenUrl = 'https://github.com/login/oauth/access_token';
         const params = new URLSearchParams({
             client_id: this.clientId,
             client_secret: this.clientSecret,
             code,
-            redirect_uri: process.env.GITHUB_REDIRECT_URI,
+            redirect_uri: this.redirectURI,
         });
 
         const response = await fetch(tokenUrl, {
@@ -75,8 +62,7 @@ $
             throw new Error('Failed to retrieve GitHub user information');
         }
 
-        const userData = await response.json();
-        return userData;
+        return await response.json();
     }
 }
 
